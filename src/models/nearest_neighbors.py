fixed bug in numpy masking logic to give correct ATE

src/models/nearest_neighbors.py
@@ -112,8 +112,8 @@ def find_neighbors(df, nearest_dict, k):
 
 
 def calc_y0(votes, dists, d):
-    mask = np.ma.masked_where(dists < d, votes) # mask values outside distance range
-    result = mask.mean(axis=1)
+    m = np.ma.masked_where(dists > d, votes) # mask values outside distance range
+    result = m.mean(axis=1)
     return result
 
 def make_final_data(df, train, treatment, k, d):
@@ -126,8 +126,8 @@ def make_final_data(df, train, treatment, k, d):
 
     # Get the final results
     control = calc_y0(votes, dists, d)
-    final_df = final_df[control.mask]
-    final_df['y0'] = control[control.mask]
+    final_df = final_df[~control.mask]
+    final_df['y0'] = control[~control.mask]
     final_df = final_df.merge(df[['ncid', 'voted']], how='inner', on='ncid').rename({'voted': 'y1'}, axis=1)
     final_df['ate'] = final_df['y1'] - final_df['y0']
     return final_df

src/models/test_params.py
@@ -23,12 +23,15 @@ if __name__ == '__main__':
                      show_default=True,
                      type=float)
     
-    distances = list(range(0.1, 2, 0.1))
+    distances = list(np.arange(0.1, 2., 0.1))
     
     df = load_and_sort(filepath)
     treatment = df.index[df['poll_changed'] == 1].values
     train, test = train_test_split(treatment, test_size=test_size, random_state=1337)
     
+    avg_effects = []
+    std_errors = []
+    row_counts = []
     for d in distances:
         final_df = make_final_data(df, train, treatment, k, d)
         # Do stuff here to measure ATE, SE, nrows...? 